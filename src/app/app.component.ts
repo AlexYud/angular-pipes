bdb761b9a142f2233d06fc3bc28b60dbb0e3bbf5
Alexandre Yudi <alexyud@hotmail.com>
2023-12-24 19:37:24 -0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ConvertPipe } from './convert.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ConvertPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string = '';
+  date: string = '';
+  amount: number = 0;
+  miles: number = 0;
+
+  onNameChange(target: any) {
+    this.name = target.value;
+  }
+
+  onDateChange(target: any) {
+    this.date = target.value;
+  }
+
+  onAmountChange(target: any) {
+    this.amount = target.value;
+  }
+
+  onMilesChange(target: any) {
+    this.miles = target.value;
+  }
 }
